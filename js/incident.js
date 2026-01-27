5e5a436fb59ccfdabd9f169eba5d8decc5794d1a
alnasrstarsecurity-nss <alnasrstarit@gmail.com>
2026-01-27 16:03:34 +0300

    Update incident.js

--- js/incident.js
+++ js/incident.js
@@ -1,7 +1,7 @@
 /* ===============================
    CONFIG
 ================================ */
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxIDypSCZyWXrKP7MCPsc9FUpdcOU2XoVL2TOCTypAzPRXfEM4ALy3Ys4-YKoqQSbgo/exec";
+const SCRIPT_URL = "https://s";
 
 const form = document.getElementById("incidentform");
 const status = document.getElementById("status");
