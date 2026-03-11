64b2021917314abae09bfdcd0d7e47542e031b29
alnasrstarsecurity-nss <alnasrstarit@gmail.com>
2026-03-11 13:21:02 +0300

    Update deviation.js

--- js/deviation.js
+++ js/deviation.js
@@ -190,7 +190,6 @@ form.addEventListener("submit", async e => {
          document.getElementById("SupEmpNumber").value = loginempnumber;
          document.getElementById("Supdesignation").value = logindesi;
          
-         document.getElementById("empStatus").textContent = "";
          submitBtn.disabled = false;
         
         clearSupSignature();
