ba8a6e6590c137449971f76b9088384663fe6574
alnasrstarsecurity-nss <alnasrstarit@gmail.com>
2026-03-16 14:17:51 +0300

    Update manpowerupdate.js

--- js/manpowerupdate.js
+++ js/manpowerupdate.js
@@ -3,7 +3,7 @@
 /* ===============================
    CONFIG
 ================================ */
-const SCRIPT_URL = "";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxVqNDrQtXKGIa6tG5DtqHdWYDujFyiGwM5svouoRR98TGB-WvBLjLjQEWy2nU1kpHxug/exec";
 
 const form = document.getElementById("manpower");
 const status = document.getElementById("status");
