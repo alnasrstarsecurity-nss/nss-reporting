37f7daf27b96eadc1fdc4d16456710a6b8b051c0
alnasrstarsecurity-nss <alnasrstarit@gmail.com>
2026-03-17 12:48:58 +0300

    Update manpowerupdate.js

--- js/manpowerupdate.js
+++ js/manpowerupdate.js
@@ -3,7 +3,7 @@
 /* ===============================
    CONFIG
 ================================ */
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxlC8kNi8GE5AdL5IX2o7jkzGL0HCbipK4kkEilCzuZzbd0pOJeZ_j1_4uR9kTIws3juQ/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyiNbQk7irdUA5eoQlc-4xwEpUNqgw_z9dVzoNw2ULWCZPyaqrUEWbJ7rcwdhfg_AIX7A/exec";
 
 const form = document.getElementById("manpower");
 const status = document.getElementById("status");
